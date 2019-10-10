51813f85ac2917fd8e5ac501183fc914f5c6f884
Justin Martin <jlmartin432@gmail.com>
2019-10-10 12:16:34 -0700

    First commit of gif-maker

---
+++ generative_gif_maker.py
@@ -0,0 +1,32 @@
+from PIL import Image
+import imageio
+import colorsys
+import numpy
+from string import Template
+
+
+startFile = 'lenna.png'
+destinationFile = 'lenna.gif'
+imagePath = Template('./assets/$fileName')
+gifPath = Template('./gifs/$fileName')
+image = Image.open(imagePath.substitute(fileName=startFile))
+numpyImage = numpy.asarray(image)
+
+width, height = image.size
+pixels = image.load()
+images = []
+
+for k in range(20):
+    for i in range(width):
+        for j in range(height):
+            r, g, b = pixels[i, j]
+
+            r = (r + 13) % 256
+            g = (g + 13) % 256
+            b = (b + 13) % 256
+
+            pixels[i, j] = (r, g, b)
+
+    images.append(numpy.asarray(image))
+
+imageio.mimsave(gifPath.substitute(fileName=destinationFile), images)
